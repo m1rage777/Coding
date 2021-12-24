d2e283d9f32d78294530373a299bfb1a11f00bb4
m1rage777 <m1rage777@vk.com>
2021-12-24 13:25:12 +0700

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   Lesson_1.py
    	new file:   Lesson_2.py
    	new file:   Lesson_3.py
    	new file:   Lesson_4.py
    	new file:   foxford.py

---
+++ Lesson_1.py
@@ -0,0 +1,21 @@
+# print('Hello world!!!')
+# print('Hello,'+ 'M1rage')
+# print('3+2')
+# print(3+2)
+# print('Hello, \' world')
+# print('Hello, "\\"  world')
+# print('Hello,','world', "and", 'M1rage')
+# print('Hello,','world', "and", 'M1rage', sep="-")
+# print('Hello,','world', "and", 'M1rage', sep="")#  Без разделителя
+# print("Hello \nM1rage")
+
+#1) Выведите с помощью двух функций print два предложения: «Я прохожу курс по Python» и «Я учу Python по курсу: ‘Программирование на Python с Нуля до Гуру’».
+#2) Выведите с помощью только одного вызова функции print два предложения из предыдущего пункта, причём после первого предложения должен быть переход на новую строку.
+
+print('Я прохожу курс по Python!!!')
+print("Я учу курс Python по курсу: \'Программирование на Python c Нуля до Гуру\'")
+print("Я учу курс Python по курсу: \n\'Программирование на Python c Нуля до Гуру\'")
+
+
+
+
---
+++ Lesson_2.py
@@ -0,0 +1,42 @@
+# age = 25 #integer
+# print("Возраст: " + str(age))
+# print("Возраст:", age)
+#
+# temp = 5.9; #float
+# print("Температура:", temp, "градусов")
+#
+# username = "M1rage" #string
+# print("Имя пользователя", username); print("Имя пользователя", username)
+#
+# isexists = True #Boolean
+# print("Существует:", isexists)
+# isexists = False #Boolean
+# print("Существует:", isexists)
+#
+# age = "18.5"
+# print("Возраст: " + age)
+#
+# print("Тип переменной age: ", type(age))
+# print("Тип переменной temp: ", type(temp))
+# print("Тип переменной isexists: ", type(isexists))
+# print("Тип переменной integer: ", type(10))
+
+chislo = 10
+print(chislo)
+chislo = 15
+print("Значение переменной chislo = ", chislo)
+
+stroka1 = "Значение переменной"
+stroka2 = "равно"
+print(stroka1,stroka2,chislo)
+
+pravda = True
+chislo = pravda
+pizdej = False
+chislo = pizdej
+print(stroka1,stroka2,pravda,pizdej,chislo)
+
+
+
+
+
