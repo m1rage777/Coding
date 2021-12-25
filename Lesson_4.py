6a455aedce58141eb8b3a4ec5265ded4f888a838
Stanislav <m1rage777@vk.com>
2021-12-25 09:56:30 +0700

    замена

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,3 +1,51 @@
-print("Вот и проверим PyCharm & Git")
-print("Вроде всё пашет нормально")
-print("Пробуем то же самое из терминала в пичарме")
+# x = 59
+# y = 20
+# print("x = ", x)
+# print("y = ", y)
+#
+# #Основные математические операции
+# print("x + y =", x + y)
+# print("x - y =", x - y)
+# print("x * y =", x * y)
+# print("x / y =", x / y)
+# print("x // y =", x // y)
+# print("x % y =", x % y)
+# print("2 ** 3 =", 2 ** 3)
+# print("(2 + 3 * 3) * 2 =", (2 + 3 * 3) * 2)
+#
+# #Битовые операции
+# x = 5
+# y = 7
+# print("x = ", x, "=", bin(x))
+# print("x = ", x, "=", hex(x))
+# print("20 =", hex(20))
+# print("20 = ", oct(20))
+# print("10 = ", oct(10))
+#
+# print("--------------------")
+# print("x =", x, "=", bin(x))
+# print("y =", y, "=", bin(y))
+# print("x | y =", bin(x | y))
+# print("x & y =", bin(x & y))
+# print("x ^ y =", bin(x ^ y))
+# print("~x =", bin(~x))
+# print("x << 1 =", bin(x << 1))
+# print("x >> 1 =", bin(x >> 1))
+
+x = 27
+y = 54
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x // y =", x // y)
+print("x % y =", x % y)
+print("x * y =", x * y)
+
+z = 13
+chislo = z
+print("Выводим в двоичной системе ","=", bin(chislo))
+
+
+
+
